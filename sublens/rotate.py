0682f2eec39a13cb3f85ed4f825bff7435031a63
vtn <vargatn@gmail.com>
2016-03-08 17:57:25 +0100

    added great circle distance..

--- sublens/rotate.py
+++ sublens/rotate.py
@@ -1,6 +1,31 @@
 import numpy as np
 
 
+def gcdist(pa, pb, mode='simple', deg=True):
+    """Great circle distance"""
+
+    if deg:
+        pa = np.array(pa, copy=True, dtype='float64') * np.pi / 180.
+        pb = np.array(pb, copy=True, dtype='float64') * np.pi / 180.
+
+    if mode == 'simple':
+        val = _sphere_dist(pa, pb)
+    else:
+        raise NotImplementedError
+
+    if deg:
+        val *= 180. / np.pi
+
+    return val
+
+
+def _sphere_dist(pa, pb):
+    """Simple great circle dist in RADIANS"""
+    dl = np.abs(pa[0] - pb[0])
+    dsigm = np.arccos(np.sin(pa[1]) * np.sin(pb[1]) + np.cos(pa[1]) * np.cos(pb[1]) * np.cos(dl))
+    return dsigm
+
+
 def rotmatr(angle, axis_vector, deg=True):
     """
     3D rotation matrix around specified axis_vector
@@ -25,9 +50,9 @@ def rotmatr(angle, axis_vector, deg=True):
 
 def spher2cart(spher, deg=True):
     """Spherical to cartesian"""
-    spher = spher.copy()
+    spher = np.array(spher, copy=True, dtype='float64')
     if deg:
-        spher *= np.pi / 180
+        spher *= np.pi / 180.
     x = np.cos(spher[0]) * np.cos(spher[1])
     y = np.sin(spher[0]) * np.cos(spher[1])
     z = np.sin(spher[1])
@@ -38,7 +63,7 @@ def spher2cart(spher, deg=True):
 
 def cart2spher(cart, deg=True):
     """Cartesian to Spherical"""
-    cart = cart.copy()
+    cart = np.array(cart, copy=True, dtype='float64')
     r = np.sqrt(np.sum(cart ** 2.))
     az = np.arctan2(cart[1], cart[0])
     lat = np.arcsin(cart[2] / r)
@@ -49,12 +74,12 @@ def cart2spher(cart, deg=True):
     return spher
 
 
-def rot_center(rd, cent, angle=180, deg=True):
+def rot_center(rd, cent, angle=180., deg=True):
     """
-    Rotates the rd point around the axis defined by the center point cent
+    Rotates the rd point around the axis defined by the coordinate pair cent
 
     :param rd: point pair (RA, DEC)
-    :param cent:rotation center, default is (0, 0)
+    :param cent:rotation center (RA, DEC)
     :return: rotated position (RA, DEC)
     """
 
