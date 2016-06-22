a077bc085177182a190092440ecff6c92f9bd251
Tamas Norbert Varga <vargatn@gmail.com>
2016-06-22 17:52:26 +0200

    data revision wont trough

--- sublens/data/__init__.py
+++ sublens/data/__init__.py
@@ -11,7 +11,21 @@ Contents:
      (Tested to work well for DES redmagic galaxies)
 
 * rotate:
-    -
+    - Exact Spherical rotations
+    - Great circle distance
+    - cart to polar transformation and the reverse
+
+* shearops:
+    - calculate bin edges, centers and areas
+    - Handle Raw profiles read from file
+    - Creat stacked profiles
+    - add and subtract profiles with their proper JackKnife covariance
+    - Measure cross-covariance of several profiles
+
+* troughs:
+    - Handle trough maps
+    - query targets in the vicinity of troughs
+    - create matched cluster -- trough coordinate tables
 
 """
 
