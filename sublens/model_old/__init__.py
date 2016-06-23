4ff7c7d074ef8aca3b1adb963b36be6ae83e0e43
Tamas Norbert Varga <vargatn@gmail.com>
2016-06-23 22:14:11 +0200

    getting out in the better end of things...

--- sublens/model_old/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Shear profile modeling
-"""
-
-# from .astroconvert import *
-# from .partable import *
-# from .profiles import *
-#
--- sublens/model_old/cycalc/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Cython-based calculations
-"""
-
-import pyximport; pyximport.install()
-from ..cycalc.trunc_nfw import tnfw
-from ..cycalc.trunc_nfw import tnfw_ring
-
