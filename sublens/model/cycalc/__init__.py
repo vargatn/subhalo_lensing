4b91c5c55a849d68c4a3b5e1d153751821bc38ff
vtn <vargatn@gmail.com>
2016-09-02 17:34:21 +0200

    more addition

--- sublens/model/cycalc/__init__.py
+++ sublens/model/cycalc/__init__.py
@@ -2,6 +2,6 @@
 Cython-based calculations
 """
 
-# import pyximport; pyximport.install()
-# from ..cycalc.trunc_nfw import tnfw
-# from ..cycalc.trunc_nfw import tnfw_ring
+import pyximport; pyximport.install()
+from ..cycalc.trunc_nfw import tnfw
+from ..cycalc.trunc_nfw import tnfw_ring
